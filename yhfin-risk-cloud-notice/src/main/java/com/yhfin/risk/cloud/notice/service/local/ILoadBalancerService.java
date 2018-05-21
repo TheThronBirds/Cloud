861ffe42cd44513075eac4503a212c6dab59ba8c
caohui <caohui@yhfin.com>
2018-05-21 09:31:23 +0800

    loadbanlance

---
+++ yhfin-risk-cloud-notice/src/main/java/com/yhfin/risk/cloud/notice/service/local/ILoadBalancerService.java
@@ -0,0 +1,27 @@
+/****************************************************
+ * 创建人：  @author caohui    
+ * 创建时间: 2018年5月21日/上午9:29:44
+ * 项目名称: yhfin-risk-cloud-notice 
+ * 文件名称: ILoadBalancerService.java
+ * 文件描述: @Description 通过轮询方式发送同步内存 同步条目消息
+ * 公司名称: 深圳市赢和信息技术有限公司
+ *
+ * All rights Reserved, Designed By 深圳市赢和信息技术有限公司
+ * @Copyright:2016-2018
+ *
+ ********************************************************/
+package com.yhfin.risk.cloud.notice.service.local;
+
+ /**
+ * 通过轮询方式发送同步内存 同步条目消息
+ * 包名称：com.yhfin.risk.cloud.notice.service.local
+ * 类名称：ILoadBalancerService
+ * 类描述：通过轮询方式发送同步内存 同步条目消息
+ * 创建人：@author caohui
+ * 创建时间：2018/5/13/12:20
+ */
+public interface ILoadBalancerService {
+	
+	
+	
+}
